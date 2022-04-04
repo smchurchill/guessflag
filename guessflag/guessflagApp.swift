19fea6b45119017f474c4d75bf35bf6458c5242d
Samuel Churchill <meadowschurchill@gmail.com>
2022-04-04 12:41:14 -0700

    Initial Commit

---
+++ guessflag/guessflagApp.swift
@@ -0,0 +1,17 @@
+//
+//  guessflagApp.swift
+//  guessflag
+//
+//  Created by sam on 2022-04-04.
+//
+
+import SwiftUI
+
+@main
+struct guessflagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
